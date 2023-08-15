6df362956132c77cb9cd6d21da2f079dac054a1f
pettersand <petter.sand@gmail.com>
2023-08-15 19:56:50 +0200

    login session path fix

--- helper.py
+++ helper.py
@@ -18,7 +18,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user_id' not in session:
-            return redirect(url_for('login', next=request.url))
+            return redirect(url_for('salloc.login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
 
